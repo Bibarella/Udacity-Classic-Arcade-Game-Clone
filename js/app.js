15ffde580ff402e954b8fd38cdb10f61a0a2d081
Bianca Glasner <neotrinity1501@yahoo.de>
2019-02-01 14:46:12 +0100

    added Multipilation for any movements by dt parameter

--- js/app.js
+++ js/app.js
@@ -16,6 +16,11 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+	this.x = this.x + this.speed *dt;
+	if (this.x >= 505) {
+		this.x = 0;
+	}
+	this.checkCollision();
 };
 
 // Draw the enemy on the screen, required method for game
